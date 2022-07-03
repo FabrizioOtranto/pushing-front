d2fef47673e46550597e7cfc7a5028fa24548edc
FabrizioOtranto <Fabryotranto@gmail.com>
2022-07-03 00:18:53 -0300

    Se rediseño el componente de OnlineShop

--- src/components/OnlineShop/OnlineShop.jsx
+++ src/components/OnlineShop/OnlineShop.jsx
@@ -1,6 +1,5 @@
-import React, { useContext, useState,   } from 'react';
+import React, { useContext, useState, } from 'react';
 import { Navigate } from 'react-router-dom';
-
 import '../../App.css';
 import {
     Heading,
@@ -12,6 +11,7 @@ import Navbar from '../Navbar';
 import ShoppingCart from "./ShoppingCart"
 import Products from "./Products"
 import BuyForm from './BuyForm';
+import SuccessBuy from './SuccessBuy';
 
 const formInitialState = {
     firstName: '',
@@ -23,47 +23,134 @@ const OnlineShop = () => {
     const [shopingCartProduct, setShopingCartProduct] = useState([])
     const { isOpen, onOpen, onClose } = useDisclosure()
     const [formInfo, setformInfo] = useState(formInitialState);
-    
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [cardNumber, setCardNumber] = useState('')
+    const [showShoppingcart, setShowShoppingCart] = useState(false)
+    const [showProductsList, setShwoProductsList] = useState(true)
+    const [showBuyForm, setShowBuyForm] = useState(false)
+    const [errorMessage, setErrorMessage] = useState("")
 
     if (!token) {
         return <Navigate to="/" replace={true} />;
     }
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setformInfo({
-            ...formInfo,
-            [name]: value,
-        });
-    };
-
     const handleClick = (e) => {
         e.preventDefault();
-        setShopingCartProduct([
-            ...shopingCartProduct,
-            {
-                name: e.target.value,
-                id: e.target.id,
-            },
-        ]);
+        var shopingCartArray = []
+        shopingCartProduct.map(elem => {
+            shopingCartArray.push(elem.name)
+        })
+        if (!shopingCartArray.includes(e.target.value)) {
+            setShopingCartProduct([
+                ...shopingCartProduct,
+                {
+                    name: e.target.value,
+                    id: e.target.id,
+                },
+            ]);
+            alert(`${e.target.value} has been added to the shopping cart`)
 
+        }
+        else {
+            alert(`${e.target.value} is already in the shoping cart. You cannot add the same product twice`)
+        }
+    }
+    const handleShowShoppingcart = () => {
+        setShowShoppingCart(true)
+        setShwoProductsList(false)
+    }
+
+    const handleGoToProducts = () => {
+        setShowShoppingCart(false)
+        setShwoProductsList(true)
+        setShowBuyForm(false)
+        onClose()
+    }
+    const handleShowBuyForm = () => {
+        setShowShoppingCart(false)
+        setShowBuyForm(true)
+    }
+    const handleCancelPurchase = () => {
+        setShowShoppingCart(true)
+        setShowBuyForm(false)
     }
 
     const handleDelete = (id) => {
         setShopingCartProduct(shopingCartProduct.filter((shopCartProduct) => shopCartProduct.id !== id));
     };
-    const handleSubmit = () => {
-        
+
+    const handleChange = (e) => {
+        if (e.target.name == "firstName") {
+            setFirstName(e.target.value);
+        }
+        else if (e.target.name == "lastName") {
+            setLastName(e.target.value);
+        }
+        else {
+            setCardNumber(e.target.value);
+        }
+    };
+
+    const handleFirstNameValidation = () => {
+        let formIsValid = true;
+
+        if (typeof firstName !== 'undefined') {
+            if (!firstName.match(/^[a-zA-Z]+$/)) {
+                formIsValid = false;
+                setErrorMessage('First name cannot have numbers or special characters');
+                return formIsValid;
+            }
+        }
+        return formIsValid;
+    };
+
+    const handleLastNameValidation = () => {
+        let formIsValid = true;
+
+        if (typeof lastName !== 'undefined') {
+            if (!lastName.match(/^[a-zA-Z]+$/)) {
+                formIsValid = false;
+                setErrorMessage('Last name cannot have numbers or special characters');
+                return formIsValid;
+            }
+        }
+        return formIsValid;
+    };
+
+    const handleCardNumberValidation = () => {
+        let formIsValid = true;
+
+        if (typeof cardNumber !== 'undefined') {
+            if (!cardNumber.match(/^[0-9]{16,16}$/)) {
+                formIsValid = false;
+                setErrorMessage('Card number must have 16 characters');
+                return formIsValid;
+            }
+        }
+        return formIsValid;
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (handleFirstNameValidation() && handleLastNameValidation() && handleCardNumberValidation()) {
         setformInfo([
-            ...formInfo,
             {
-                firstName: formInfo.firstName,
-                lastName: formInfo.lastName,
-                cardNumber: formInfo.cardNumber
+                firstName: firstName,
+                lastName: lastName,
+                cardNumber: cardNumber
             },
         ]);
+        onOpen();
+    }
+    }
 
-
+    const handleFinishProcess = () => {
+        setShowShoppingCart(false)
+        setShwoProductsList(true)
+        setShowBuyForm(false)
+        onClose()
+        setShopingCartProduct([])
     }
 
     return (
@@ -75,13 +162,18 @@ const OnlineShop = () => {
             <Flex justify="center" direction="column" m="20">
                 <Products
                     handleClick={handleClick}
+                    showProductsList={showProductsList}
+                    handleShowShoppingcart={handleShowShoppingcart}
                 >
 
                 </Products>
                 <ShoppingCart
                     shopingCartProduct={shopingCartProduct}
                     handleDelete={handleDelete}
-                    onOpen={onOpen}
+                    showShoppingcart={showShoppingcart}
+                    handleShowBuyForm={handleShowBuyForm}
+                    handleGoToProducts={handleGoToProducts}
+
                 >
                 </ShoppingCart>
                 <BuyForm
@@ -89,9 +181,22 @@ const OnlineShop = () => {
                     onClose={onClose}
                     handleSubmit={handleSubmit}
                     handleChange={handleChange}
-                    formInfo={formInfo}
+                    firstName={firstName}
+                    lastName={lastName}
+                    cardNumber={cardNumber}
+                    showBuyForm={showBuyForm}
+                    handleCancelPurchase={handleCancelPurchase}
+                    handleGoToProducts={handleGoToProducts}
+                    errorMessage={errorMessage}
                 >
                 </BuyForm>
+                <SuccessBuy
+                    formInfo={formInfo}
+                    isOpen={isOpen}
+                    onClose={onClose}
+                    shopingCartProduct={shopingCartProduct}
+                    handleFinishProcess={handleFinishProcess}
+                ></SuccessBuy>
             </Flex>
         </>
     );
--- src/components/OnlineShop/SuccessBuy.jsx
+++ src/components/OnlineShop/SuccessBuy.jsx
@@ -1,25 +1,46 @@
 import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button,
     Text,
-
 } from '@chakra-ui/react';
 import React from 'react';
-import Navbar from '../Navbar';
 
 
-const SuccessBuy = ({ 
-
- }) => {
+const SuccessBuy = ({ isOpen, onClose, formInfo, shopingCartProduct, handleFinishProcess }) => {
     return (
         <>
-        <Navbar></Navbar>
-            <Text
-                fontSize={70}
-                color={"secondary.500"}
-                id="message"
-                name="message"
-                fontWeight={"bolder"}>
-                You have finished buying, the products will be delivered soon
-            </Text>
+            {formInfo.length ? (
+                <Modal isOpen={isOpen} onClose={onClose} closeOnOverlayClick={false}>
+                    <ModalOverlay />
+                    <ModalContent>
+                        <ModalHeader
+                            m={"1"}
+                        >Purchase has been completed successfully</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                            <Text>{formInfo[0].firstName} {formInfo[0].lastName} has succesfully purchased the following items</Text>
+
+                            {shopingCartProduct.map((shopCartProduct) => (
+                                <Text key={shopCartProduct.id}>{shopCartProduct.name}</Text>
+                            ))}
+                            <Text>The credit card used was {formInfo[0].cardNumber}</Text>
+                            
+                        </ModalBody>
+                        <ModalFooter>
+                        <Button bg={"secondary.500"} mr={3} onClick={handleFinishProcess}>
+                                Thank you
+                            </Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </Modal>
+            ) : null
+            }
         </>
 
     );
