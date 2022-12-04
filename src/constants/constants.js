0cc0acfe807186893e2b87a523849574bc046f36
Fabrizio Otranto <Fabryotranto@gmail.com>
2022-12-04 19:47:14 -0300

    fix endpoint

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://pushing-it-backend.herokuapp.com/api';
+export const BASE_URL = 'https://pushing-it.onrender.com/api';
 
 //
 export const GENDER = ['Male', 'Female', 'Other'];
